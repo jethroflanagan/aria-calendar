d3b892ecd8c7bb59f079bf086979af8c7aae86b1
Jethro Flanagan <jethroflanagan@gmail.com>
2025-08-18 14:49:06 +0200

    Init

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import { useState } from "react";
+import "./App.css";
+import Calendar from "./components/DatePicker";
+
+function App() {
+  return (
+    <>
+      <Calendar />
+    </>
+  );
+}
+
+export default App;
